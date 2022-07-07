fc9f3bb6cb03c181414c0b7dd41d0d16a553fc1b
greysoh <greysoh@pm.me>
2022-07-07 11:18:01 -0700

    feature: Adds table of content generation.

--- index.js
+++ index.js
@@ -31,11 +31,69 @@ if (!fs.existsSync("out/template.html")) {
 console.log("DirectoryInit: Reading template file...");
 const template = fs.readFileSync("out/template.html", "utf8");
 
-const files = libs.recursiveGetDirectories("out");
-
 console.log(
   "Convert: (REMINDER) If you are planning to use code highlighting, you must add a 'highlight.js' theme css file to the 'template.html' file."
 );
+
+console.log("TableGen: Checking if there are any table of content files...");
+
+for (i of libs.recursiveGetDirectories("out")) {
+  if (i.endsWith("toc.gen")) {
+    console.log("TableGen: Found a request to make a table of content file.");
+    console.log("TableGen: Deleting generation files...");
+
+    const tocOriginal = i;
+
+    libs.forceRecursiveDelete(i);
+    libs.forceRecursiveDelete(i.replace("toc.gen", "toc.md"));
+
+    console.log("TableGen: Getting directory list...");
+    const dirList = libs.recursiveGetDirectories(
+      i.replace("/toc.gen", "").replace("\\toc.gen", "").replace("toc.gen", "")
+    );
+
+    let toc = "# Table of Contents\n\n- [Table of Contents](toc.html)\n";
+
+    console.log("TableGen: Generating table of contents...");
+
+    for (j of dirList) {
+      if (j.endsWith(".md")) {
+        console.log("TableGen: Found '" + j + "'.");
+        const file = fs.readFileSync(j, "utf8");
+
+        const title = file
+          .split("\n")[0]
+          .replace("# ", "")
+          .replace("## ", "")
+          .replace("### ", "")
+          .replace("#", "")
+          .replace("##", "")
+          .replace("###", "");
+
+        let link = tocOriginal;
+        
+        link = link.replace("\\toc.gen", "").replace("/toc.gen", ""); // Makes a filter
+        link = j.replace(link, ""); // Removes the path
+        link = link.replace(".md", ".html"); // Replaces the extension
+        link = link.replace("\\", "/"); // Replaces the backslash with a forward slash
+        link = link.replace("/", ""); // Remove the first forward slash
+        
+        toc += "- [" + title + "](" + link + ")\n";
+      }
+    }
+
+    console.log("TableGen: Writing table of contents file...");
+
+    fs.writeFileSync(
+      tocOriginal.replace("toc.gen", "toc.md"),
+      prettier.format(toc, { parser: "markdown" })
+    );
+
+    console.log("TableGen: Done with table of contents file.");
+  }
+}
+
+const files = libs.recursiveGetDirectories("out");
 
 for (i of files) {
   if (i.endsWith(".md")) {
@@ -52,7 +110,7 @@ for (i of files) {
       parser: "html",
     });
 
-    console.log("Convert: Writing '" + i + "' to HTML...");
+    console.log("Convert: Writing '" + i + "'...");
     fs.writeFileSync(i.replace(".md", ".html"), newHTML);
 
     console.log("Convert: Deleting '" + i + "'...");
